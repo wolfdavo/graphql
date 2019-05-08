7d765d0d0cf9edefb1f476519184568e19e3a753
David Wolfenden <wolfdavo@gmail.com>
2019-05-08 12:52:22 -0500

    Just a periodic commit

--- server/app.js
+++ server/app.js
@@ -2,7 +2,13 @@ const express = require('express'); //reqire express
 const graphqlHTTP = require('express-graphql'); //Express doesnt know what to do with graphql quries by default, so we require this package that it can hand the queries off to.
 const schema = require('./schema/schema');
 const app = express(); //making an express app(server)
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb+srv://learning:graphql@learning-graphql-5ewmn.gcp.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
+
+mongoose.connection.once('open', () => {
+    console.log('connected to database')
+})
 //this is saying 'we got a hit on the /graphql endpoint, i dont know what to do with it so im gonna give it to graphqlHTTP which is a function that handles gql requests.'
 //it takes an argument of an object
 app.use('/graphql', graphqlHTTP({
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,19 +1,32 @@
 const graphql = require('graphql'); //importing graphql
 const _ = require('lodash');
+const Book = require('../model/book');
+const Author = require('../model/author');
 
 const { 
     GraphQLObjectType,
     GraphQLString, 
     GraphQLSchema,
-    GraphQLID
+    GraphQLID,
+    GraphQLInt,
+    GraphQLList
      } = graphql; // deconstrucing graphql tp take out what we need 
 
 //dummy array for testing before an actual database 
-var books = [
-    {author:'steve',id:"1"},
-    {author:'barry',id:"2"},
-    {author:'john',id:"3"}
-]
+// var books = [
+//     {name: 'Name of the Wind',genre: 'Fantasy',id:"1", authorid: '1'},
+//     {name: 'The Final Empire',genre: 'Fantasy',id:"2", authorid: '2'},
+//     {name: 'The Long Earth',genre: 'Sci-Fi',id:"3", authorid: '3'},
+//     {name: 'the Hero of Ages',genre: 'Fantasy',id:"4", authorid: '2'},
+//     {name: 'The Color of Magic',genre: 'Fantasy',id:"5", authorid: '3'},
+//     {name: 'The Light Fantastic',genre: 'Sci-Fi',id:"6", authorid: '3'}
+// ];
+
+// var authors = [
+//     {name: 'Patric Rothfuss', age: 44, id:'1'},
+//     {name: 'Brandon Sanderson', age: 42, id:'2'},
+//     {name: 'Terry Pratchett', age: 66, id:'3'}
+// ]
 
 //Defining a graphql object. This is our book object.
 const BookType = new GraphQLObjectType({
@@ -21,9 +34,32 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        genre: {type: GraphQLString}
+        genre: {type: GraphQLString},
+        authorid: {type: GraphQLID},
+        author: {
+            type: AuthorType,
+            // resolve(parent, args){
+            //     return _.find(authors, {id: parent.authorid})
+            // }
+        }
     })
 })
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt},
+        books: {
+            type: new GraphQLList(BookType),
+            // resolve(parent, args){
+            //     return _.filter(books, {authorid: parent.id})
+            // }
+        }
+    })
+})
+
 
 
 //Root quries are landing points for graphql requests. 
@@ -33,9 +69,67 @@ const RootQuery = new GraphQLObjectType({
         book: {
             type: BookType,
             args: {id: {type: GraphQLID}},
+            // resolve(parent, args){
+            //     console.log(args.id)
+            //     //code to get data from db or other data source 
+            //     return _.find(books, {id:args.id})
+            // }
+        },
+        author: {
+            type: AuthorType,
+            args: {id: {type: GraphQLID}},
+            // resolve(parent, args){
+            //     return _.find(authors, {id: args.id})
+            // }
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            // resolve(parent, args){
+            //     return books
+            // }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            // resolve(parent, args){
+            //     return authors
+            // }
+        }
+    }
+})
+
+
+//thos is how data is changed 
+const Mutations = new GraphQLObjectType({
+    name: 'Mutator',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: {type: GraphQLString},
+                age: {type: GraphQLInt}
+            },
             resolve(parent, args){
-                //code to get data from db or other data source 
-                return _.find(books, {id:args.id})
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                })
+                return author.save();
+            }
+        },
+        addBook: {
+            type: BookType,
+            args:  {
+                name: {type: GraphQLString},
+                authorid: {type: GraphQLID},
+                genre: {type: GraphQLString}
+            },
+            resolve(parent, args){
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorid: args.authorid
+                });
+                return book.save();
             }
         }
     }
@@ -44,5 +138,6 @@ const RootQuery = new GraphQLObjectType({
 
 //Exporting as a graphql schema with one query as our root query 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutations
 })
